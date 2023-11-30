b9ed6d6bd122de8eaf8d6c6beb9cfb62cccab188
carpspet <121141414+carpspet@users.noreply.github.com>
2023-11-30 17:10:03 -0500

    new main
    changed file read in type

--- main.cpp
+++ main.cpp
@@ -1,1 +1,48 @@
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+bool isValidAlgorithm(const std::string& algorithm) {
+    return algorithm == "Booyer Moore" || algorithm == "Rabin Karp";
+}
+
+void processAlgorithm(std::string& algorithm) {
+    do {
+        std::cout << "Enter the name of the algorithm you would like to use, Booyer Moore, or Rabin Karp" << std::endl;
+        std::cin >> algorithm;
+
+        if (!isValidAlgorithm(algorithm)) {
+            std::cout << "Invalid algorithm. Please enter either 'Booyer Moore' or 'Rabin Karp'." << std::endl;
+        }
+
+    } while (!isValidAlgorithm(algorithm));
+}
+
+int main(int argc, char* argv[]) {
+    
+    std::string algorithm;
+
+    std::ifstream inputFile(argv[1]);
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening file: " << argv[1] << std::endl;
+        return 1;
+    }
+
+    //reading file into a string 
+    std::stringstream buffer;
+    buffer << inputFile.rdbuf();
+    std::string fileContent = buffer.str();
+
+    inputFile.close(); 
+
+    processAlgorithm(algorithm);
+    
+    //optional
+    //std::cout << "File Content: " << fileContent << std::endl;
+    std::cout << "Selected Algorithm: " << algorithm << std::endl;
+
+    return 0;
+}
