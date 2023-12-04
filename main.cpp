d0d99d96b061e0fb2497345adc5b067523d9155a
carpspet <121141414+carpspet@users.noreply.github.com>
2023-12-03 21:32:09 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,17 +3,21 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "rabin_karp.h"
-#include "booyer_moore.h"
+#include "boyer_moore.h"
+#include "bench.h"
 
 bool isValidAlgorithm(std::string& algorithm) {
-    return algorithm == "Booyer Moore" || algorithm == "Rabin Karp" || algorithm == "both" || algorithm == "run a benchmark";
+    return algorithm == "Boyer Moore" || algorithm == "Rabin Karp" || algorithm == "both" || algorithm == "run a benchmark";
 }
 
 void processAlgorithm(std::string& algorithm) {
     do {
-        std::cout << "Please enter what you would like to run. Your choices are: Booyer Moore, Rabin Karp, both, or run a benchmark" << std::endl;
+        std::cout << "Please enter what you would like to run. Your choices are: Boyer Moore, Rabin Karp, both, or run a benchmark" << std::endl;
         std::cin >> algorithm;
+
+        std::transform(algorithm.begin(), algorithm.end(), algorithm.begin(), ::tolower);
 
         if (!isValidAlgorithm(algorithm)) {
             std::cout << "Invalid algorithm. Please check your spelling and spacing." << std::endl;
@@ -27,8 +31,6 @@ int main(int argc, char* argv[]) {
     std::string result;
     std::string secondResult;
     std::string algorithm;
-
-    float benchmarkResult;
 
     std::ifstream inputFile(argv[1]);
 
@@ -50,35 +52,57 @@ int main(int argc, char* argv[]) {
     //std::cout << "File Content: " << fileContent << std::endl;
     std::cout << "Selected Algorithm: " << algorithm << std::endl;
 
+    bench benchmark;
     TwoWayQ twoWayQueue;
-    if (algorithm == "Booyer Moore"){
+    if (algorithm == "Boyer Moore"){
         std::string pattern;
-        std::string booyerMoore_target;
+        std::string boyerMoore_target;
 
-        std::cout << "Enter desired search pattern";
+        std::cout << "Enter desired search pattern" << std::endl;
         std::cin >> pattern;
 
-        std::cout << "Enter the target string: ";
-        std::cin >> booyerMoore_target;
+        std::cout << "Enter the target string: " << std::endl;
+        std::cin >> boyerMoore_target;
 
         BoyerMoore boyerMoore(pattern);
 
-        boyerMoore.search(booyerMoore_target);
+        boyerMoore.search(boyerMoore_target);
         boyerMoore.printResults();
     }
     else if(algorithm == "Rabin Karp"){
         std::string targetString;
-        std::cout << "Enter the target string: ";
+        std::cout << "Enter the target string: " << std::endl;
         std::cin >> targetString;
         
         std::vector<int>* rabinKarpResult = twoWayQueue.rabin_karp(argv[1], targetString);
         delete rabinKarpResult;
     }
     else if(algorithm == "both"){
-        
+        std::string targetString;
+        std::string pattern;
+    
+        std::cout << "Enter target string" << std::endl;
+        std::cin >> targetString;
+
+        std::cout << "Enter desired search pattern (Boyer Moore)" << std::endl;
+        std::cin >> pattern;
+
+
+        std::vector<int>* rabinKarpResult = twoWayQueue.rabin_karp(argv[1], targetString);
+        delete rabinKarpResult;
+
+        BoyerMoore boyerMoore(pattern);
+
+        boyerMoore.search(targetString);
+        boyerMoore.printResults();
     }
     else if(algorithm == "run a benchmark"){
+        std::string benchmark_targetString;
+        std::cout << "Enter target string" << std::endl;
+        std::cin >> benchmark_targetString;
 
+        benchmark.benchRK(argv[1], benchmark_targetString);
+        benchmark.benchBM(argv[1], benchmark_targetString);
     }
 
     return 0;
