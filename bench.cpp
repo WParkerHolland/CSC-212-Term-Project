56885be061d9d7eedc52cd064060a644f5adb508
Owen Cunnie <118402981+owenc740@users.noreply.github.com>
2023-12-04 14:05:39 -0500

    Add files via upload
    Fixed benchmark files

--- bench.cpp
+++ bench.cpp
@@ -1,1 +1,27 @@
+// TermProj / bench.cpp
 
+#include "bench.h"
+#include <iostream>
+#include <ctime>
+
+void bench::benchRK(std::string& fileName, std::string& targetString) {
+
+    clock_t startTime = clock(); // start clock
+    TwoWayQ rabinKarpObj; // run rabin-karp
+    rabinKarpObj.rabin_karp(fileName, targetString);
+    clock_t endTime = clock(); // end clock
+    double timeRK = double(endTime - startTime) / CLOCKS_PER_SEC; // calculate seconds
+
+    std::cout << "Rabin-Karp Execution Time: " << timeRK << " seconds" << std::endl; // output time
+}
+
+void bench::benchBM(std::string& fileName, std::string& targetString) {
+
+    clock_t startTime = clock(); // start clock
+    BoyerMoore boyerMooreObj(fileName); // run boyer-moore
+    boyerMooreObj.search(targetString);
+    clock_t endTime = clock(); // end clock
+    double timeBM = double(endTime - startTime) / CLOCKS_PER_SEC; // calculate seconds
+
+    std::cout << "Boyer-Moore Execution Time: " << timeBM << " seconds" << std::endl; // output time
+}
