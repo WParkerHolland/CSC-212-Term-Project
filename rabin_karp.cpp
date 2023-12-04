cd5bcaf85091a149587056c50134220690f4d1c7
Parker Holland <parkerholland04@gmail.com>
2023-12-04 16:52:41 -0500

    Added comments and updated ReadMe
    Comments explaining functions in rabin_karp.cpp were added. Additionally, the ReadMe was updated to match the changes to the program.

--- rabin_karp.cpp
+++ rabin_karp.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include "rabin_karp.h"
 
+// Constructor for Node Class
 QueueNode::QueueNode(char data, int row, int col, QueueNode* last, QueueNode* next){
     this->data = data;
     loc[0] = row;
@@ -17,16 +18,23 @@ QueueNode::~QueueNode(){
 
 }
 
+// Hash function
+// Data is the character to take the hash value of
+// Returns the hash value of the provided character
 int TwoWayQ::hash(char data){
     return (int(data) * int(pow(7, int(data) % 11))) % 2027;
 }
 
+// Default constructor for queue class
+// Starts with an empty queue
 TwoWayQ::TwoWayQ(){
     head = nullptr;
     tail = nullptr;
     len = 0;
 }
 
+// Desctructor for queue class
+// Iterates through each node in the queue and deletes it
 TwoWayQ::~TwoWayQ(){
     QueueNode* grim = head;
     QueueNode* reaper = nullptr;
@@ -39,6 +47,9 @@ TwoWayQ::~TwoWayQ(){
     tail = nullptr;
 }
 
+// Creates a new node at the head of the queue
+// The variables passed to the function are used to create the node
+// Returns the hash value of the character inside the node
 int TwoWayQ::enqueue(char data, int row, int col){
     if(this->head == nullptr){
         head = new QueueNode(data, row, col, nullptr, nullptr);
@@ -52,6 +63,9 @@ int TwoWayQ::enqueue(char data, int row, int col){
     return tempHash;
 }
 
+// Deletes the node at the tail of the queue
+// If there is a tail node, the hash value of the character inside the node is returned
+// If the queue is empty, -1 is returned, which is not a number than can't be produced by the hash function
 int TwoWayQ::dequeue(){
     if(tail != nullptr){
         int tempHash = this->hash(tail->data);
@@ -63,7 +77,10 @@ int TwoWayQ::dequeue(){
     return -1;
 }
 
-
+// Rabin-Karp String Search Algorithm
+// Searches for every instance of targetString in the file named fileName
+// Returns a vector of rows and columns, each pair pointing to a place where that string can be found in the file.
+// If the targetString cannot be found, a null pointer is returned instead.
 std::vector<int>* TwoWayQ::rabin_karp(std::string fileName, std::string targetString){
     std::fstream legFile;
     legFile.open(fileName, std::ios::in);
