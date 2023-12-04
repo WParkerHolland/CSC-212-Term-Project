ed2548c6959c30381584c1448dfea81aeaa30a04
Owen Cunnie <118402981+owenc740@users.noreply.github.com>
2023-12-03 21:12:42 -0500

    Add files via upload
    Updated benchmark code

--- bench.cpp
+++ bench.cpp
@@ -1,1 +1,25 @@
+// TermProj / bench.cpp
 
+#include "bench.h"
+#include <iostream>
+#include <ctime>
+
+void bench::benchRK(const std::string& fileName, const std::string& targetString) {
+
+    clock_t startTime = clock(); // start clock
+    TwoWayQ rabinKarpObj; // run rabin-karp
+    clock_t endTime = clock(); // end clock
+    double timeRK = double(endTime - startTime) / CLOCKS_PER_SEC; // calculate seconds
+
+    std::cout << "Rabin-Karp Execution Time: " << timeRK << " seconds" << std::endl; // output time
+}
+
+void bench::benchBM(const std::string& fileName, const std::string& targetString) {
+
+    clock_t startTime = clock(); // start clock
+     // run boyer-moore
+    clock_t endTime = clock(); // end clock
+    double timeBM = double(endTime - startTime) / CLOCKS_PER_SEC; // calculate seconds
+
+    std::cout << "Boyer-Moore Execution Time: " << timeBM << " seconds" << std::endl; // output time
+}
--- bench.h
+++ bench.h
@@ -1,1 +1,21 @@
+// TermProj / bench.h
+#include "rabin_karp.h"
+#include "boyer_moore.h"
+#include <iostream>
 
+#ifndef CSC212_BENCH_H
+#define CSC212_BENCH_H
+
+
+class bench {
+private:
+
+public:
+
+    void benchRK(const std::string& fileName, const std::string& targetString);
+    void benchBM(const std::string& fileName, const std::string& targetString);
+
+};
+
+
+#endif //CSC212_BENCH_H
