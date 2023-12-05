37b22ee480ef19e9666d5e7fb24483b1128507c4
c4ddyshack <99035233+c4ddyshack@users.noreply.github.com>
2023-12-04 19:39:39 -0500

    updated header file for boyer-moore
    changed boyer_moore.h

--- boyer_moore.h
+++ boyer_moore.h
@@ -8,21 +8,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 class BoyerMoore{
     public:
-        BoyerMoore(std::string& userInput);
-        void search(std::string& givenText);
+        BoyerMoore(std::string& userInput, std::string& fname);
+        void search();
+        std::string givenText;
         void printResults();
     private:
         int patternLength;
         std::vector<int> indexes;
         std::string pattern;
-        std::string givenText;
-
+        
         std::vector<int> badCharTable;
         std::vector<int> goodSuffixTable;
 
+        void readFile(std::string& fname);
         void bad_char_compute();
         void good_suffix_compute();
         int shiftCalc(int a, int b);
